3d2caa916ad7eb3a1f9076f77bba75051ac5ddb0
chrissamanage <chris.walls@samanage.com>
2017-11-02 12:42:48 -0400

    Adding new functionality

--- lib/samanage/api/requester.rb
+++ lib/samanage/api/requester.rb
@@ -1,6 +1,6 @@
 module Samanage
 	class Api
-		def get_requester_id(value: nil)
+		def get_requester_id(value: nil, name: nil, email: nil)
 			api_call = self.execute(path: "requesters.json?name=#{value}")
 			requester = api_call[:data].size == 1 ? api_call[:data][0] : nil
 			requester
