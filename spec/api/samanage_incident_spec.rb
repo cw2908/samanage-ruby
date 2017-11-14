33354374af9766d3e164d6630ae54edea06ef3f7
chrissamanage <chris.walls@samanage.com>
2017-11-14 14:45:43 -0500

    Better incident check

--- spec/api/samanage_incident_spec.rb
+++ spec/api/samanage_incident_spec.rb
@@ -63,14 +63,14 @@ describe Samanage::Api do
 			it 'update_incident: update_incident by id' do
 				incidents = @controller.collect_incidents
 				sample_id = incidents.sample['id']
-				new_name = (0...50).map { ('a'..'z').to_a[rand(26)] }.join
+				description = (0...500).map { ('a'..'z').to_a[rand(26)] }.join
 				incident_json = {
 					:incident => {
-						:name => new_name
+						:description => description
 					}
 				}
 				incident_update = @controller.update_incident(payload: incident_json.to_json, id: sample_id)
-				expect(incident_update[:data]["name"]).to eq(new_name)
+				expect(incident_update[:data]['description']).to eq(new_name)
 				expect(incident_update[:code]).to eq(200).or(201)
 			end
 		end
