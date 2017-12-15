6465d943d17534683fb77354a75e2d8ebbd286f1
chrissamanage <chris.walls@samanage.com>
2017-12-15 15:01:54 -0500

    Updating spec

--- spec/api/samanage_category_spec.rb
+++ spec/api/samanage_category_spec.rb
@@ -8,13 +8,12 @@ describe Samanage::Api do
 		it 'collects all categories' do
 			puts "Category methods: #{@controller.methods}"
 			categories = @controller.collect_categories
-			category_count = categories[:total_count]
 			expect(categories).to be_an(Array)
 		end
+
 		it 'creates a category' do
-			category_name = "category ##{(rand*10**4).ceil}"
-			category_location = "Location #{(rand*10**4).ceil}"
-			category_description = "Location #{(rand*10**4).ceil}"
+			category_name = "Category Name ##{(rand*10**4).ceil}"
+			category_description = "Descrption #{(rand*10**4).ceil}"
 			payload = {
 				category: {
 					name: category_name,
