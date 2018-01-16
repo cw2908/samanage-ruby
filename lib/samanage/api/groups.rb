2c6c9c8ffa101bf6cbb1440038fd54900685de00
Chris Walls <cwalls2908@gmail.com>
2018-01-16 11:06:40 -0500

    Group patch

--- lib/samanage/api/groups.rb
+++ lib/samanage/api/groups.rb
@@ -22,7 +22,7 @@ module Samanage
 
 		def find_group_id_by_name(group: nil)
 			group_api = self.execute(path: "groups.json?name=#{group}")
-			if group_api[:data].size == 1 && group == group_api[:data].first['name']
+			if group == group_api[:data].first['name']
 				return group_api[:data].first['id']
 			end
 		end
